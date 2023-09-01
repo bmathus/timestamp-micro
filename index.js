97119064c72967b4fac6eaae1fa6f7468121d9ad
bmathus <bojko.matus@gmail.com>
2023-09-01 21:02:17 +0200

    v1.2

--- index.js
+++ index.js
@@ -62,3 +62,5 @@ app.use((req, res) => {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+module.exports = app;
