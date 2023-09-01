46c112f8a0138bffa500e55f9bf1d95f13c1be57
bmathus <bojko.matus@gmail.com>
2023-09-01 21:14:20 +0200

    v1.3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 var express = require('express');
+const path = require('path');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -11,7 +12,7 @@ var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
